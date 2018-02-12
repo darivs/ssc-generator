b80331b30d204fd39efd5d233ab8bbbe642707db
Darius <darius.tack@gmail.com>
2018-02-12 13:27:29 +0100

    Add files via upload

---
+++ SSCGen/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace SSCGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtB_code = new System.Windows.Forms.RichTextBox();
+            this.tB_store = new System.Windows.Forms.TextBox();
+            this.lb_ = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tB_img = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tB_op = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tB_treasure = new System.Windows.Forms.TextBox();
+            this.bt_gen = new System.Windows.Forms.Button();
+            this.bt_copy = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tB_eg = new System.Windows.Forms.TextBox();
+            this.cB_tc = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // rtB_code
+            // 
+            this.rtB_code.Enabled = false;
+            this.rtB_code.Location = new System.Drawing.Point(431, 15);
+            this.rtB_code.Margin = new System.Windows.Forms.Padding(4);
+            this.rtB_code.Name = "rtB_code";
+            this.rtB_code.Size = new System.Drawing.Size(405, 259);
+            this.rtB_code.TabIndex = 0;
+            this.rtB_code.Text = "";
+            // 
+            // tB_store
+            // 
+            this.tB_store.Location = new System.Drawing.Point(12, 41);
+            this.tB_store.Margin = new System.Windows.Forms.Padding(4);
+            this.tB_store.Name = "tB_store";
+            this.tB_store.Size = new System.Drawing.Size(405, 22);
+            this.tB_store.TabIndex = 1;
+            // 
+            // lb_
+            // 
+            this.lb_.AutoSize = true;
+            this.lb_.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_.ForeColor = System.Drawing.Color.White;
+            this.lb_.Location = new System.Drawing.Point(8, 15);
+            this.lb_.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_.Name = "lb_";
+            this.lb_.Size = new System.Drawing.Size(93, 24);
+            this.lb_.TabIndex = 2;
+            this.lb_.Text = "Store Link";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(8, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Banner-Image Link";
+            // 
+            // tB_img
+            // 
+            this.tB_img.Location = new System.Drawing.Point(12, 95);
+            this.tB_img.Margin = new System.Windows.Forms.Padding(4);
+            this.tB_img.Name = "tB_img";
+            this.tB_img.Size = new System.Drawing.Size(405, 22);
+            this.tB_img.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(8, 122);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Originalpreis";
+            // 
+            // tB_op
+            // 
+            this.tB_op.Location = new System.Drawing.Point(12, 148);
+            this.tB_op.Margin = new System.Windows.Forms.Padding(4);
+            this.tB_op.Name = "tB_op";
+            this.tB_op.Size = new System.Drawing.Size(201, 22);
+            this.tB_op.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(8, 176);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Treasure Link";
+            // 
+            // tB_treasure
+            // 
+            this.tB_treasure.Location = new System.Drawing.Point(12, 202);
+            this.tB_treasure.Margin = new System.Windows.Forms.Padding(4);
+            this.tB_treasure.Name = "tB_treasure";
+            this.tB_treasure.Size = new System.Drawing.Size(405, 22);
+            this.tB_treasure.TabIndex = 7;
+            // 
+            // bt_gen
+            // 
+            this.bt_gen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_gen.Location = new System.Drawing.Point(13, 281);
+            this.bt_gen.Margin = new System.Windows.Forms.Padding(4);
+            this.bt_gen.Name = "bt_gen";
+            this.bt_gen.Size = new System.Drawing.Size(407, 46);
+            this.bt_gen.TabIndex = 10;
+            this.bt_gen.Text = "Generate Code";
+            this.bt_gen.UseVisualStyleBackColor = true;
+            this.bt_gen.Click += new System.EventHandler(this.bt_gen_Click);
+            // 
+            // bt_copy
+            // 
+            this.bt_copy.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_copy.Location = new System.Drawing.Point(432, 281);
+            this.bt_copy.Margin = new System.Windows.Forms.Padding(4);
+            this.bt_copy.Name = "bt_copy";
+            this.bt_copy.Size = new System.Drawing.Size(407, 46);
+            this.bt_copy.TabIndex = 11;
+            this.bt_copy.Text = "Copy Code";
+            this.bt_copy.UseVisualStyleBackColor = true;
+            this.bt_copy.Click += new System.EventHandler(this.bt_copy_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(214, 122);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 24);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "elite*gold";
+            // 
+            // tB_eg
+            // 
+            this.tB_eg.Location = new System.Drawing.Point(218, 148);
+            this.tB_eg.Margin = new System.Windows.Forms.Padding(4);
+            this.tB_eg.Name = "tB_eg";
+            this.tB_eg.Size = new System.Drawing.Size(201, 22);
+            this.tB_eg.TabIndex = 12;
+            // 
+            // cB_tc
+            // 
+            this.cB_tc.AutoSize = true;
+            this.cB_tc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cB_tc.ForeColor = System.Drawing.Color.White;
+            this.cB_tc.Location = new System.Drawing.Point(12, 237);
+            this.cB_tc.Name = "cB_tc";
+            this.cB_tc.Size = new System.Drawing.Size(152, 28);
+            this.cB_tc.TabIndex = 14;
+            this.cB_tc.Text = "Trading-Cards";
+            this.cB_tc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(849, 348);
+            this.Controls.Add(this.cB_tc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tB_eg);
+            this.Controls.Add(this.bt_copy);
+            this.Controls.Add(this.bt_gen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tB_treasure);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tB_op);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tB_img);
+            this.Controls.Add(this.lb_);
+            this.Controls.Add(this.tB_store);
+            this.Controls.Add(this.rtB_code);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtB_code;
+        private System.Windows.Forms.TextBox tB_store;
+        private System.Windows.Forms.Label lb_;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tB_img;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tB_op;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tB_treasure;
+        private System.Windows.Forms.Button bt_gen;
+        private System.Windows.Forms.Button bt_copy;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tB_eg;
+        private System.Windows.Forms.CheckBox cB_tc;
+    }
+}
+
---
+++ SSCGen/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSCGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_gen_Click(object sender, EventArgs e)
+        {
+            string tc = "";
+
+            if (cB_tc.Checked)
+            {
+                tc = "[IMG]http://www.elitepvpers.com/images/tbm/trading/tick.png[/IMG]";
+            }
+
+            rtB_code.Text = @"[URL=""" + tB_store.Text + "\"" + "][IMG]" + tB_img.Text + "[/IMG][/URL] | " + tB_op.Text +" | [CENTER]" + tB_eg.Text + " [IMG]http://www.elitepvpers.com/images/tbm/gold.gif[/IMG][/CENTER] | " + tc + "| [URL=\"" + tB_treasure.Text + "\"][IMG]http://www.elitepvpers.com/images/tbm/treasures.gif[/IMG][/URL]";
+        }
+
+        private void bt_copy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(rtB_code.Text);
+        }
+    }
+}
